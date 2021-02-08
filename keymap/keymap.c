1b58a11d46ac8f6ec35bdc8f76514904778db013
alexmisk <alexmisk@ya.ru>
2021-02-08 18:08:02 +0300

    feat: add CTRL/TAB key on the default layer

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -69,6 +69,7 @@ enum planck_keycodes {
 #define PASTE LGUI(KC_DOT)
 #define RMWRD LALT(KC_BSPC)
 #define RMLINE LGUI(KC_BSPC)
+#define CTL_TAB LCTL_T(KC_TAB)
 
 // Brackets and punctuation
 #define CURLL LSFT(KC_MINS)
@@ -81,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_planck_grid(
 KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+CTL_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
 KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
 KC_F18,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   MOUSE,    KC_SPC,  RAISE,   WIN50L,  WIN50R,  XXXXXXX, KC_ENT
 ),
