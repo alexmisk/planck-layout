082352938ce1d001fcf1053cd1ee11eb47ce186d
alexmisk <alexmisk@ya.ru>
2021-03-01 21:59:42 +0300

    feat: add FUNCTION layer and rearrange BASE/LOWER

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -22,6 +22,7 @@ enum planck_layers {
   _BASE,
   _LOWER,
   _RAISE,
+  _FUNCT,
   _MOUSE,
   _ADJUST
 };
@@ -30,6 +31,7 @@ enum planck_layers {
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define MOUSE LT(_MOUSE, KC_SPC)
+#define FUNCT MO(_FUNCT)
 #define ADJUST MO(_ADJUST)
 
 // Window management via Magnet app
@@ -48,8 +50,8 @@ enum planck_layers {
 #define TIMER HYPR(KC_T) // Toggle Tyme2
 
 // Player shortcuts
-#define PREV KC_MRWD
-#define NEXT KC_MFFD
+#define PREV KC_MEDIA_PREV_TRACK
+#define NEXT KC_MEDIA_NEXT_TRACK
 #define VOL_DN KC_VOLD
 #define VOL_UP KC_VOLU
 #define PLAY KC_MPLY
@@ -103,21 +105,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
 CTL_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
 KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
-KC_F18,  KC_LCTL, KC_LALT, KC_LGUI, TD(LOW_ALF),   MOUSE,    KC_SPC,  RAISE,   WIN50L,  WIN50R,  XXXXXXX, KC_ENT
+KC_F18,  KC_LCTL, KC_LALT, KC_LGUI, TD(LOW_ALF),   MOUSE,    KC_SPC,  RAISE,   FUNCT,  XXXXXXX,  XXXXXXX, KC_ENT
 ),
 
 [_LOWER] = LAYOUT_planck_grid(
-TIMER,   TYPOGR,  ADD_TSK, CLIPBRD, SCRSHT,  CURLL,    CURLR,   KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_PSLS,
-PREV,    NEXT,    VOL_DN,  PLAY,    VOL_UP,  RUCOMMA,  RUDOT,   KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_PAST,
-_______, SEL_ALL, CUT,     COPY,    PASTE,   KC_MINS,  KC_EQL,  KC_P1,   KC_P2,   KC_P3,   KC_PEQL, KC_PDOT,
-_______, _______, _______, _______, _______, _______,  _______, KC_P0,   WIN33L,  WIN66R,  WIN100,  _______
+TIMER,   TYPOGR,  ADD_TSK, CLIPBRD, SCRSHT,  CURLL,    CURLR,   KC_P7,  KC_P8,   KC_P9,   KC_PMNS, KC_PSLS,
+PREV,    NEXT,    VOL_DN,  PLAY,    VOL_UP,  RUCOMMA,  RUDOT,   KC_P4,  KC_P5,   KC_P6,   KC_PPLS, KC_PAST,
+_______, SEL_ALL, CUT,     COPY,    PASTE,   KC_MINS,  KC_EQL,  KC_P1,  KC_P2,   KC_P3,   KC_PEQL, KC_PDOT,
+_______, _______, _______, _______, _______, _______,  _______, KC_P0,  _______, _______, _______, _______
 ),
 
 [_RAISE] = LAYOUT_planck_grid(
-KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-_______, KC_GRV,  KC_CIRC, KC_AMPR, KC_ASTR, KC_QUES,  _______, KC_BSPC, RMWRD,   RMLINE,  _______, _______,
+XXXXXXX, KC_TILD, KC_NUBS, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, WIN50L,  WIN100,  WIN50R,  XXXXXXX, XXXXXXX,
+KC_CAPS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+CURLL,   KC_GRV,  KC_CIRC, KC_AMPR, KC_ASTR, KC_QUES,  _______, KC_BSPC, RMWRD,   RMLINE,  _______, CURLR,
 _______, _______, _______, _______, ADJUST,  _______,  _______, _______, _______, _______, _______, _______
+),
+
+[_FUNCT] = LAYOUT_planck_grid(
+XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX,
+XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX,
+XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,
+XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 [_MOUSE] = LAYOUT_planck_grid(
